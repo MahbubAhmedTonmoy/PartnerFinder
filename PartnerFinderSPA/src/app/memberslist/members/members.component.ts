d99734c48e0956e6f65732ddc4f7d0c72fb05cd8
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-07-30 08:41:09 +0600

    paging filter design not working

--- PartnerFinderSPA/src/app/memberslist/members/members.component.ts
+++ PartnerFinderSPA/src/app/memberslist/members/members.component.ts
@@ -10,17 +10,46 @@ import { AlertifyService } from '../../service/alertify.service';
 })
 export class MembersComponent implements OnInit {
  users: User[];
- pageNumber = 1;
- pageSize = 3;
+ genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+ pageSizeList = [{value: 5,  display: '5'}, {value: 10, display: '10'}, {value: 15, display: '15'}, {value: 20, display: '20'}]
+ user: User = JSON.parse(localStorage.getItem('user'));
+ userParams: any = {};
+ pagingParams: any = {};
+ 
   constructor(private userService: UserService, private alert: AlertifyService) { }
 
   ngOnInit() {
     this.loadUsers();
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+    this.pagingParams.pageSize = this.pageSizeList[0];
+    this.pagingParams.pageNumber = 1;
   }
 
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
+  left(){
+    if(this.pagingParams.pageNumber == 1) return;
+    else{
+      this.pagingParams.pageNumber  = this.pagingParams.pageNumber - 1
+    }
+  }
+  right(){
+    if(this.pagingParams.pageNumber == 100) return;
+    else{
+      this.pagingParams.pageNumber  = this.pagingParams.pageNumber + 1
+    }
+  }
 
   loadUsers(){
-    this.userService.getUsers(this.pageNumber, this.pageSize).subscribe((users: User[]) =>{
+    this.userService.getUsers(this.pagingParams.pageNumber, this.pagingParams.pageSize).subscribe((users: User[]) =>{
       this.users = users;
       console.log(this.users);
     }, error => {
